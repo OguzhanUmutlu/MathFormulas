32c88b4252f7526bd20ad975ff21db838eb9e914
Oğuzhan <64753457+OguzhanUmutlu@users.noreply.github.com>
2022-01-12 23:17:53 +0300

    init

--- motion from 2d to 2d.js
+++ motion from 2d to 2d.js
@@ -1,1 +1,1 @@
--sin((((atan2(x1 - x2, y1 - y2) / PI * 180) - 90) * PI / 180) - (PI / 2)), -cos((((atan2(x1 - x2, y1 - y2) / PI * 180) - 90) * PI / 180) - (PI / 2))
+new Vector2(-sin((((atan2(x1 - x2, y1 - y2) / PI * 180) - 90) * PI / 180) - (PI / 2)), -cos((((atan2(x1 - x2, y1 - y2) / PI * 180) - 90) * PI / 180) - (PI / 2)))
